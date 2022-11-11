e3c0a529ddaecd41388993818c4a41f34eb5efb5
wingdust <995315823@qq.com>
2022-11-11 18:23:58 +0800

    re

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -888,3 +888,43 @@ endfunction
 
 ]])
 -- set matchpairs+=<template>:</template>
+--
+
+local function get_visual_selection()
+  local s_start = vim.fn.getpos("'<")
+  local s_end = vim.fn.getpos("'>")
+  local n_lines = math.abs(s_end[2] - s_start[2]) + 1
+  local lines = vim.api.nvim_buf_get_lines(0, s_start[2] - 1, s_end[2], false)
+  lines[1] = string.sub(lines[1], s_start[3], -1)
+  if n_lines == 1 then
+    lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3] - s_start[3] + 1)
+  else
+    lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3])
+  end
+  return table.concat(lines, '\n')
+end
+
+
+function snake_case()
+    -- expand('<cword>') to get the current word under cursor
+    local current_word = vim.call('expand','<cword>') 
+    -- change case
+    -- local snake_case_word = string.gsub(current_word, "(%u)", "_%1" )
+    -- diw delete in word
+    -- i insert
+    -- and add snake_case_word
+    -- vim.cmd("normal! diwi" ..  snake_case_word)
+end
+
+
+vim.api.nvim_create_user_command(
+  'OpenURL', 
+  function(opts) 
+    -- print(opts)
+    -- print(get_visual_selection())
+    print(snake_case())
+    -- cmd = ("open itter.js")
+    -- os.execute(cmd)
+  end,
+  { nargs = '*' }
+  )
