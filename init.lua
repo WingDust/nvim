633d7c6bdd0b673a62a7cd32811adf3126dfdf9e
wingdust <995315823@qq.com>
2023-01-09 20:27:03 +0800

    g map

--- init.lua
+++ init.lua
@@ -40,6 +40,17 @@ function print_r ( t )
     print()
 end
 
+
+function map(mode, lhs, rhs, opts)
+  local options = { noremap = true, silent = true }
+  if opts then
+    options = vim.tbl_extend('force', options, opts)
+  end
+  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+-- opt = { noremap = true, silent = true }
+
+
 -- Import Lua modules
 -- require ('fun')
 -- require('packer_init')
